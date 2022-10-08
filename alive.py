9f35d6e0098a10825a156e6195626cb32fa69e06
msgsuryabot20 <115179559+msgsuryabot20@users.noreply.github.com>
2022-10-08 22:09:14 +0530

    Add files via upload

--- alive.py
+++ alive.py
@@ -1,22 +1,19 @@
-from time import sleep
-from requests import get as rget
-from os import environ
-from logging import error as logerror
+# Implement By - @anasty17 (https://github.com/SlamDevs/slam-mirrorbot/commit/0bfba523f095ab1dccad431d72561e0e002e7a59)
+# (c) https://github.com/SlamDevs/slam-mirrorbot
+# All rights reserved
 
-BASE_URL = environ.get('BASE_URL_OF_BOT', None)
+import time
+import requests
+import os
+
+BASE_URL = os.environ.get('BASE_URL_OF_BOT', None)
 try:
     if len(BASE_URL) == 0:
-        raise TypeError
-    BASE_URL = BASE_URL.rstrip("/")
-except TypeError:
+        BASE_URL = None
+except:
     BASE_URL = None
-PORT = environ.get('PORT', None)
+PORT = os.environ.get('PORT', None)
 if PORT is not None and BASE_URL is not None:
     while True:
-        try:
-            rget(BASE_URL).status_code
-            sleep(600)
-        except Exception as e:
-            logerror(f"alive.py: {e}")
-            sleep(2)
-            continue
+        time.sleep(600)
+        status = requests.get(BASE_URL).status_code
